567e1fc86b1d1df9e86efebd3ef6e30db914b11a
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-07-06 14:25:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,9 +8,13 @@ while ! pg_isready -h ${PGHOST} -p ${PGPORT}; do sleep 0.25; done;
 
 echo Database is now available
 
-bundle exec rails db:chatwoot_prepare
+bundle exec rails db:setup
 
 bundle exec rails db:migrate
+
+bundle exec rails packages:update 
+
+bundle exec rails s -b  0.0.0.0 -p 3000"
 
 multirun \
     "bundle exec sidekiq -C config/sidekiq.yml" \
