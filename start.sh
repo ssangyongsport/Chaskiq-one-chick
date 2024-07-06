b00fbf9eebe3e57e69d44cf7d9098f5bf52e5190
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-07-06 14:42:19 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,22 +2,8 @@
 
 set -e
 
-echo Waiting for database...
-
-while ! pg_isready -h ${PGHOST} -p ${PGPORT}; do sleep 0.25; done; 
-
-echo Database is now available
-
 bundle exec rails db:setup
-
 bundle exec rails db:migrate
-
 bundle exec rails packages:update 
 
-bundle exec rails s -b  0.0.0.0 -p 3000"
-
-multirun \
-    "bundle exec sidekiq -C config/sidekiq.yml" \
-    "bundle exec rails s -b 0.0.0.0 -p $PORT"
-
-false
+bundle exec rails s -b 0.0.0.0 -p 3000
